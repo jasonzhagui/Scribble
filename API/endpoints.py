853cc02592a351721be63354d41285e1bdbc1f30
Jason Zhagui <jasonzhagui@gmail.com>
2021-10-22 19:09:01 -0400

    updated to main

--- API/endpoints.py
+++ API/endpoints.py
@@ -38,7 +38,7 @@ class ListRoom(Resource):
         """
         Returns a list of all chat rooms.
         """
-        return {"Software Engineering": {"num_users": 49},
+        return {"Software Engineering": {"num_users": 59},
                 "AI": {"num_users": 27}, }
 
 
