27333a1d80c07bca2805fde1607f6564f6503a89
Jason Zhagui <jasonzhagui@gmail.com>
2021-10-22 19:06:11 -0400

    changed branch

--- API/endpoints.py
+++ API/endpoints.py
@@ -38,7 +38,7 @@ class ListRoom(Resource):
         """
         Returns a list of all chat rooms.
         """
-        return {"Software Engineering": {"num_users": 30},
+        return {"Software Engineering": {"num_users": 49},
                 "AI": {"num_users": 27}, }
 
 
