4bf182841486164872826148f8d50c096475f38b
abdel-b <ab7975@nyu.edu>
2021-11-16 17:52:32 -0500

    separate test db directory

--- db/db.py
+++ db/db.py
@@ -9,12 +9,14 @@ import os
 
 SCRIBBLE_HOME = os.environ["SCRIBBLE_HOME"]
 TEST_MODE = os.environ.get("TEST_MODE", 0)
-DB_DIR = f"{SCRIBBLE_HOME}/db"
+
 
 if TEST_MODE:
-    ROOMS_DB = f"{DB_DIR}/test_rooms.json"
+    DB_DIR = f"{SCRIBBLE_HOME}/db/test_dbs"
 else:
-    ROOMS_DB = f"{DB_DIR}/rooms.json"
+    DB_DIR = f"{SCRIBBLE_HOME}/db"
+
+ROOMS_DB = f"{DB_DIR}/rooms.json"
 
 OK = 0
 NOT_FOUND = 1
@@ -52,4 +54,28 @@ def add_room(roomname):
     else:
         rooms[roomname] = {"num_users": 0}
         write_rooms(rooms)
+        return OK
+
+def get_users():
+    return {"Fred": {}}
+
+
+def write_users():
+    pass
+
+
+def add_user(username):
+    """
+    Add a user to the user database.
+    Until we are using a real DB, we have a potential
+    race condition here.
+    """
+    users = get_users()
+    if users is None:
+        return NOT_FOUND
+    elif username in users:
+        return DUPLICATE
+    else:
+        users[username] = {"num_users": 0}
+        write_users(users)
         return OK
