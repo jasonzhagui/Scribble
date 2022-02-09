97f8c3f51e7716e10080220530faec66fb8c98e6
abdel-b <ab7975@nyu.edu>
2022-02-09 18:23:22 -0500

    trying to query DB via python

---
+++ db/mongo_reader.py
@@ -0,0 +1,19 @@
+"""
+Get used to pymongo!
+"""
+import db_connect as dbc
+
+COLLECT_NAME = 'layers'
+
+
+client = dbc.get_client()
+print(f"{client=}")
+
+this_collect = client[dbc.db_nm][COLLECT_NAME]
+
+
+doc = client[dbc.db_nm][COLLECT_NAME].find_one({'layer': 'head'})
+#print(f"find one = {doc=}")
+print(doc["lemon"])
+
+
