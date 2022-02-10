3052cac5357a5e4018cf14a1d23ade48524a0d59
abdel-b <ab7975@nyu.edu>
2022-02-10 17:54:20 -0500

    new list heads endpoint

--- db/data.py
+++ db/data.py
@@ -53,7 +53,7 @@ def get_head_layers():
     """
     A function to return a list of all head layers.
     """
-    return dbc.fetch_all(LAYERS, HEAD)
+    return dbc.fetch_all(HEAD, HEAD)
 
 
 def get_rooms_as_dict():
