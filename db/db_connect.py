09976f0c4143a0ec44df1379a4781a4032d4d104
Jason Zhagui <jasonzhagui@gmail.com>
2022-02-10 21:57:52 -0500

    new list heads endpoint

--- db/db_connect.py
+++ db/db_connect.py
@@ -48,7 +48,7 @@ def fetch_one(collect_nm, filters={}):
     """
     Fetch one record that meets filters.
     """
-    return client["scribbleDB"]["layers"].find_one({"layer": "head"})
+    return client[db_nm][collect_nm].find_one(filters)
 
 
 def del_one(collect_nm, filters={}):
